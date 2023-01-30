be7a88dd0bbecd0865ec7987b75494cb515e71e6
Paweł Zaremba <pawzar@gmail.com>
2023-01-30 18:33:45 +0100

    Failing test for multicall encoding.

--- abi/method_test.go
+++ abi/method_test.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/defiweb/go-eth/types"
 )
 
 func TestParseMethod(t *testing.T) {
@@ -31,5 +33,51 @@ func TestParseMethod(t *testing.T) {
 				assert.Equal(t, tt.expected, m.String())
 			}
 		})
+	}
+}
+
+func TestMethod_EncodeArgs(t *testing.T) {
+	const ZeroAddr = "0x0000000000000000000000000000000000000000"
+	const FuncERC20Approve = "function approve(address spender,uint256 value) external returns (bool)"
+	const FuncTryAggregate = "function tryAggregate(bool requireSuccess, Call[] memory calls) public returns (Result[] memory returnData)"
+
+	innerFunc := MustParseMethod(FuncERC20Approve)
+
+	const TypeCall = "(address target,bytes callData)"
+	type Call struct {
+		Address types.Address
+		Data    []byte
+	}
+	require.NoError(t, addType("Call", TypeCall))
+	const TypeResult = "(bool success,bytes returnData)"
+	type Result struct {
+		Success bool
+		Data    []byte
+	}
+	require.NoError(t, addType("Result", TypeResult))
+	outerFunc := MustParseMethod(FuncTryAggregate)
+
+	data := make([]Call, 2)
+	data[0] = Call{
+		Address: types.MustHexToAddress(ZeroAddr),
+		Data:    mustFn[[]byte](t)(innerFunc.EncodeArgs(types.MustHexToAddress(ZeroAddr), 0)),
+	}
+	data[1] = Call{
+		Address: types.MustHexToAddress(ZeroAddr),
+		Data:    mustFn[[]byte](t)(innerFunc.EncodeArgs(types.MustHexToAddress(ZeroAddr), 1)),
+	}
+
+	mustFn[[]byte](t)(outerFunc.EncodeArgs(false, data))
+}
+
+func addType(name, signature string) error {
+	var err error
+	Default.Types[name], err = ParseType(signature)
+	return err
+}
+func mustFn[C any](t *testing.T) func(c C, err error) C {
+	return func(c C, err error) C {
+		require.NoError(t, err)
+		return c
 	}
 }
